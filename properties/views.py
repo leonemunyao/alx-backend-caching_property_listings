19abcf4b508cf3c3f6eb466c7cb1552529dae63e
Leone Munyao <leonemunyao@gmail.com>
2025-08-30 02:46:22 +0300

    Caching in Django. Setting up Django Project with dockerized postgresql and redis

--- properties/views.py
+++ properties/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 from .models import Property
+from django.http import JsonResponse
 
 @cache_page(60 * 15)
 def property_list(request):
 	properties = Property.objects.all()
+	if request.headers.get('Accept') == 'application/json':
+		return JsonResponse({'properties': list(properties.values())})
 	return render(request, 'properties/property_list.html', {'properties': properties})
